ed29439414309453a9e49125a3929f3fb88559e7
annewu109 <annewu109@gmail.com>
2024-07-28 13:20:23 -0400

    Added Java: scrabble-score

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,19 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String sounds = "";
+        if (number % 3 == 0)
+            sounds = sounds + "Pling";
+        if (number % 5 == 0)
+            sounds = sounds + "Plang";
+        if (number % 7 == 0)
+            sounds = sounds + "Plong";
+
+        if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0) {
+            sounds = Integer.toString(number);
+        }
+
+        return sounds;
     }
 
 }
